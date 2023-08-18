5aa6bd1df5665578c05520b10aed2e816d56bda2
MtFBella109 <130922446+MtFBella109@users.noreply.github.com>
2023-08-18 21:37:41 +0200

    Add files via upload

--- config.go
+++ config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/spf13/viper"
 )
@@ -27,7 +26,62 @@ func Config() *configstruct {
 		webapps_directory:        configfile.GetString("general.webapps_directory"),
 		locale:                   configfile.GetString("general.locale"),
 	}
+	if config.first_launch == true {
+		newconfig := writeConfig(&config)
+		config = *newconfig
+		config.first_launch = false
+		configfile.Set("general.first_launch", config.first_launch)
+		configfile.Set("general.generate_desktop_file", config.generate_desktop_file)
+		configfile.Set("general.systemwide_desktop_entry", config.systemwide_desktop_entry)
+		configfile.Set("general.webapps_directory", config.webapps_directory)
+		configfile.Set("general.locale", config.locale)
+		configfile.WriteConfig()
+	}
 
-	fmt.Println(reflect.TypeOf(config))
 	return &config
+}
+
+func writeConfig(config *configstruct) *configstruct {
+	fmt.Println("1: Generate Desktop File (Default is true)")
+	fmt.Println("2: Make the Desktop entry systemwide available (Default is false)")
+	fmt.Println("3: In which Directory should all WebApps go in? ATTENTION: This Directory has to exist, otherwise the Program will not work correctly (Default is ~/WebApps)")
+	fmt.Println("4: Change the locale (Default is en)")
+	editconf := true
+	for editconf == true {
+		fmt.Println("Type the Number of the Config you want to change or type 'done' if everything is set correctly")
+		var confuserinput string
+		switch fmt.Scanln(&confuserinput); confuserinput {
+		case "done":
+			editconf = false
+		case "1":
+			if config.generate_desktop_file == true {
+				fmt.Println("Changed now Generate Desktop file to False")
+				config.generate_desktop_file = false
+			} else {
+				fmt.Println("Changed now Generate Desktop file to True")
+				config.generate_desktop_file = true
+			}
+		case "2":
+			if config.systemwide_desktop_entry == true {
+				fmt.Println("Changed now Generate Desktop file to False")
+				config.systemwide_desktop_entry = false
+			} else {
+				fmt.Println("Changed now Generate Desktop file to True")
+				config.systemwide_desktop_entry = true
+			}
+		case "3":
+			fmt.Println("Please type the Directory where the WebApps should be created")
+			var directory string
+			fmt.Scanln(&directory)
+			config.webapps_directory = directory
+			fmt.Println("Changed to " + directory)
+		case "4":
+			fmt.Println("Type your wanted locale You can choose: 'de', 'en'")
+			var locale string
+			fmt.Scanln(&locale)
+			config.locale = locale
+			fmt.Println("Changed to " + locale)
+		}
+	}
+	return config
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 	// "runtime"
@@ -21,25 +22,30 @@ func main() {
 	fmt.Scanln(&webappName)
 	fmt.Println("Whats the URL of the WebApp?")
 	fmt.Scanln(&webappURL)
-	cmd := exec.Command("nativefier", "--name", webappName, webappURL)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
+	os.Chdir(filepath.Join(config.webapps_directory))
+	exec.Command("nativefier", "--name", webappName, webappURL).Run()
 	if config.generate_desktop_file == true {
-		exec.Command("cp", cwd+"/templates/template.desktop", "./"+webappName+".desktop")
+		exec.Command("cp", filepath.Join(cwd, "templates", "template.desktop"), filepath.Join(config.webapps_directory, webappName+".desktop")).Run()
 		desktop_file := webappName + ".desktop"
 		file, _ := os.Open(desktop_file)
 		filedata, _ := io.ReadAll(file)
 		filedata = []byte(strings.ReplaceAll(string(filedata), "$NAME", webappName))
-		filedata = []byte(strings.ReplaceAll(string(filedata), "$PATH", config.webapps_directory+"/"+webappName+"-linux-x64/"))
+		filedata = []byte(strings.ReplaceAll(string(filedata), "$PATH", config.webapps_directory+"/"+webappName+"-linux-x64"))
 		os.WriteFile(webappName+".desktop", filedata, 0644)
 		if config.systemwide_desktop_entry == true {
-			exec.Command("sudo", "mv", "./*.desktop", "/usr/share/applications/")
+			cmd := exec.Command("sudo", "mv", filepath.Join(config.webapps_directory, webappName+".desktop"), "/usr/share/applications/")
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
 		} else {
-			exec.Command("mv", "*.desktop", "~/.local/share/applications/")
+			cmd := exec.Command("mv", filepath.Join(config.webapps_directory, webappName+".desktop"), "~/.local/share/applications/")
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 		fmt.Println("WebApp was sucesfully created")
 	}
-	exec.Command("cd %s", cwd)
+	exec.Command("cd %s", cwd).Run()
 }
